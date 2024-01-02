8f548e092b31fc1ff31ec87a7473ae4b4937815e
Sofía Millán <sofiaisabellamillan16@gmail.com>
2024-01-02 15:31:44 -0500

    Documenting path and query parameters

--- src/test/java/guru/springframework/sfgrestdocsexample/web/controller/BeerControllerTest.java
+++ src/test/java/guru/springframework/sfgrestdocsexample/web/controller/BeerControllerTest.java
@@ -6,12 +6,15 @@ import guru.springframework.sfgrestdocsexample.repositories.BeerRepository;
 import guru.springframework.sfgrestdocsexample.web.model.BeerDto;
 import guru.springframework.sfgrestdocsexample.web.model.BeerStyleEnum;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Import;
 import org.springframework.http.MediaType;
+import org.springframework.restdocs.RestDocumentationExtension;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.math.BigDecimal;
@@ -20,9 +23,15 @@ import java.util.UUID;
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+
+
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.*;
+import static org.springframework.restdocs.request.RequestDocumentation.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+@ExtendWith(RestDocumentationExtension.class)
+@AutoConfigureRestDocs
 @WebMvcTest(BeerController.class)
 @ComponentScan(basePackages = "guru.springframework.sfgrestdocsexample.web.mappers")
 class BeerControllerTest {
@@ -40,8 +49,16 @@ class BeerControllerTest {
     void getBeerById() throws Exception {
         given(beerRepository.findById(any())).willReturn(Optional.of(Beer.builder().build()));
 
-        mockMvc.perform(get("/api/v1/beer/" + UUID.randomUUID().toString()).accept(MediaType.APPLICATION_JSON))
-                .andExpect(status().isOk());
+        mockMvc.perform(get("/api/v1/beer/{beerId}", UUID.randomUUID().toString())
+                        .param("iscold","yes")
+                        .accept(MediaType.APPLICATION_JSON))
+                        .andExpect(status().isOk())
+                        .andDo(document("v1/beer",
+                                pathParameters(
+                                    parameterWithName("beerId").description("UUID of desired beer to get.")
+                                ), requestParameters(
+                                        parameterWithName("iscold").description("Is beer cold query param")
+                                )));
     }
 
     @Test
---
+++ target/generated-sources/annotations/guru/springframework/sfgrestdocsexample/web/mappers/BeerMapperImpl.java
@@ -0,0 +1,37 @@
+package guru.springframework.sfgrestdocsexample.web.mappers;
+
+import guru.springframework.sfgrestdocsexample.domain.Beer;
+import guru.springframework.sfgrestdocsexample.web.model.BeerDto;
+import javax.annotation.processing.Generated;
+import org.springframework.stereotype.Component;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2024-01-02T15:24:42-0500",
+    comments = "version: 1.5.2.Final, compiler: javac, environment: Java 19.0.2 (Oracle Corporation)"
+)
+@Component
+public class BeerMapperImpl implements BeerMapper {
+
+    @Override
+    public BeerDto BeerToBeerDto(Beer beer) {
+        if ( beer == null ) {
+            return null;
+        }
+
+        BeerDto beerDto = new BeerDto();
+
+        return beerDto;
+    }
+
+    @Override
+    public Beer BeerDtoToBeer(BeerDto dto) {
+        if ( dto == null ) {
+            return null;
+        }
+
+        Beer beer = new Beer();
+
+        return beer;
+    }
+}
